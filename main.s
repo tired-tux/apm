6ce3f485579433bcf39cbfcd3bdc5f3d0bea29a3
Eli <121198893+tired-tux@users.noreply.github.com>
2023-10-23 19:14:20 +0000

    LGTM

--- main.s
+++ main.s
@@ -2,10 +2,15 @@
 .intel_syntax noprefix
 
 _start:
-    mov rdi, 6
-    mov rsi, 10
-    call add
+    mov r8, 6
+    mov r9, 10
+    add r8, r9
+    lea rsi, [r8]
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 3
+    syscall
     
     mov rax, 60
-    mov rdi, 0
+    mov rdi, 1
     syscall
