37a94f94d8c52b746d99aa9430d3977c97b8c0b3
Eli <121198893+tired-tux@users.noreply.github.com>
2023-10-23 21:53:40 +0000

    Makefiles

--- setup.mk
+++ setup.mk
@@ -1,6 +1,8 @@
-install:
+gum:
 	sudo mkdir -p /etc/apt/keyrings
 	curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
 	echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
 	sudo apt update && sudo apt install gum
 	rm install.mk
+gcc:
+	sudo apt install build-essentials -y
