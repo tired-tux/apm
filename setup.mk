4b5cb75625f244daf3f566727805f750f8ce071c
Eli <121198893+tired-tux@users.noreply.github.com>
2023-10-23 22:00:50 +0000

    LGTM

--- setup.mk
+++ setup.mk
@@ -5,5 +5,5 @@ gum:
 	curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
 	echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
 	sudo apt update && sudo apt install gum
-del: gum gcc
+del: gum
 	rm setup.mk
